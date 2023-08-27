ec0a80afe0c901523a3d5fb03f281ab143c4df3a
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-08-27 13:01:32 +0300

    feat: word count

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def word_count(text):
+    return len(text.split())
+
 with open("books/frankenstein.txt") as f:
     file_content = f.read()
     print(file_content)
+    print(f"words: {word_count(file_content)}")
