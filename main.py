2a275663d64b4f6d3bee9f7504edec7e46243023
Perttu Autti <65128229+Pertsaa@users.noreply.github.com>
2023-08-27 13:24:39 +0300

    feat: print report function

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ def letter_counts(text):
             letter_counts[c.lower()] = 0
     return letter_counts
 
+def print_report(text):
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count(text)} words found in the document\n")
+
+    counts = letter_counts(text)
+    counts_list = []
+    for c in counts:
+        if c.isalpha():
+            counts_list.append((c, counts[c]))
+    counts_list.sort(key=lambda t: t[1], reverse=True)
+
+    for e in counts_list:
+        print(f"The '{e[0]}' character was found {e[1]} times")
+
+    print(f"--- End report ---")
+
 with open("books/frankenstein.txt") as f:
     file_content = f.read()
-    print(file_content)
-    print(f"words: {word_count(file_content)}")
-    print(letter_counts(file_content))
+    print_report(file_content)
